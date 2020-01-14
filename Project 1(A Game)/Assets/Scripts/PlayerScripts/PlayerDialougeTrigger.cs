8d8c0d89612bc0ca83f960f8e8cadfb536756dde
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-01-14 13:43:06 +0530

    29th Commit..

--- Project 1(A Game)/Assets/Scripts/PlayerScripts/PlayerDialougeTrigger.cs
+++ Project 1(A Game)/Assets/Scripts/PlayerScripts/PlayerDialougeTrigger.cs
@@ -10,6 +10,8 @@ public class PlayerDialougeTrigger : MonoBehaviour
     [SerializeField] Transform GrandpaPos;
     [SerializeField] Transform Doctor;
     [SerializeField] Transform NPC2;
+    [SerializeField] Transform NPC3;
+    [SerializeField] Transform NPC4;
 
     [SerializeField] GameObject pressImage;
     [SerializeField] GameObject playGameButton;
@@ -26,6 +28,8 @@ public class PlayerDialougeTrigger : MonoBehaviour
         Doctor = GameObject.FindGameObjectWithTag("Doctor").transform;
         Player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         NPC2 = GameObject.FindGameObjectWithTag("Grandma").transform;
+        NPC3 = GameObject.FindGameObjectWithTag("Teacher").transform;
+        NPC4 = GameObject.FindGameObjectWithTag("NPC4").transform;
 
        // dialougeTrigger[0].Trigger();
         pressImage.SetActive(false);
@@ -37,6 +41,8 @@ public class PlayerDialougeTrigger : MonoBehaviour
         float distance = Vector2.Distance(transform.position, GrandpaPos.position);
         float distanceFromDoctor = Vector2.Distance(transform.position, Doctor.position);
         float distanceFromNPC2 = Vector2.Distance(transform.position, NPC2.position);
+        float distanceFromNPC3 = Vector2.Distance(transform.position, NPC3.position);
+        float distanceFromNPC4 = Vector2.Distance(transform.position, NPC4.position);
 
         //* Converstaion With Grandpa./
         if(distance <= 1 && dm.animator.GetBool("IsOpen") == false)
@@ -100,6 +106,44 @@ public class PlayerDialougeTrigger : MonoBehaviour
                 pressImage.SetActive(true);
             }
             else if (distanceFromNPC2 > 1f)
+            {
+                pressImage.SetActive(false);
+            }
+        }
+        //*
+
+        //* Conversation With teacher.
+        if(distanceFromNPC3 < 1f && Input.GetKeyDown(KeyCode.E) && dm.animator.GetBool("IsOpen") == false)
+        {
+            dialougeTrigger[4].Trigger();
+        }
+
+        if (distanceFromNPC3 < 20f)
+        {
+            if (distanceFromNPC3 <= 1f && dm.animator.GetBool("IsOpen") == false)
+            {
+                pressImage.SetActive(true);
+            }
+            else if (distanceFromNPC3 > 1f)
+            {
+                pressImage.SetActive(false);
+            }
+        }
+        //*
+
+        //* Conversation with NPC4.
+        if(distanceFromNPC4 < 1f && Input.GetKeyDown(KeyCode.E) && dm.animator.GetBool("IsOpen") == false)
+        {
+            dialougeTrigger[5].Trigger();
+        }
+
+        if (distanceFromNPC4 < 20f)
+        {
+            if (distanceFromNPC4 <= 1f && dm.animator.GetBool("IsOpen") == false)
+            {
+                pressImage.SetActive(true);
+            }
+            else if (distanceFromNPC4 > 1f)
             {
                 pressImage.SetActive(false);
             }
