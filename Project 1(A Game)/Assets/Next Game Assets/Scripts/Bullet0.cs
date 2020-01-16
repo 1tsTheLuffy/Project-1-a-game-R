9129641554a1bb95f681838c8a13901827ea6119
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-01-16 17:59:23 +0530

    36th Commit..

---
+++ Project 1(A Game)/Assets/Next Game Assets/Scripts/Bullet0.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet0 : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Project 1(A Game)/Assets/Next Game Assets/Scripts/MainPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MainPlayer : MonoBehaviour
+{
+    private Vector2 mousePos;
+
+    [SerializeField] float elapsedTime;
+    [SerializeField] float shakeDuration;
+    [SerializeField] float shakeFrequency;
+    [SerializeField] float shakeAmplitude;
+
+    [SerializeField] GameObject[] bullet;
+
+    [SerializeField] Transform[] gunPoint;
+
+    [SerializeField] CinemachineVirtualCamera virtualCamera;
+    [SerializeField] CinemachineBasicMultiChannelPerlin virtualNoiseCamera;
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Camera cam;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        cam = Camera.main;
+
+        if(virtualCamera != null)
+        {
+            virtualNoiseCamera = virtualCamera.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+        }
+    }
+
+    private void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        transform.up = direction;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bullet[0], gunPoint[0].position, gunPoint[0].rotation);
+            elapsedTime = shakeDuration;
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            Instantiate(bullet[1], gunPoint[1].position, gunPoint[1].rotation);
+            elapsedTime = shakeDuration;
+        }
+
+        Shake();
+    }
+
+    private void OnMouseDrag()
+    {
+        
+        transform.position = mousePos;
+    }
+
+    void Shake()
+    {
+        if (elapsedTime > 0)
+        {
+            virtualNoiseCamera.m_AmplitudeGain = shakeAmplitude;
+            virtualNoiseCamera.m_FrequencyGain = shakeFrequency;
+            elapsedTime -= Time.deltaTime;
+        }
+        else
+        {
+            elapsedTime = 0;
+            virtualNoiseCamera.m_FrequencyGain = 0;
+            virtualNoiseCamera.m_AmplitudeGain = 0;
+        }
+    }
+}
